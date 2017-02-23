03dbdaffa29faac51636babd32ffe22ca94bb497
Juan <jsantos@novainternet.es>
2017-02-23 09:25:06 +0100

    Initial Commit

---
+++ JSZExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSZExample
+//
+//  Created by Nova Internet on 23/2/17.
+//  Copyright © 2017 JSZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSZExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSZExample
+//
+//  Created by Nova Internet on 23/2/17.
+//  Copyright © 2017 JSZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
